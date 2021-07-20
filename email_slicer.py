e6814ed3638c07795d7ffaa0cf5c890c5a495b97
Jon Nicolas jobb <jonbon@ous-hf.no>
2021-07-20 08:45:25 +0000

    first version

--- email_slicer.py
+++ email_slicer.py
@@ -1,45 +1,33 @@
-'''
-TODO:
-Split email at @
-Assign first part to username list/csv
-Assign last part to domain list/csv
-'''
+def slice():
+    import csv
 
-import csv
-
-def slice(email):
-    #initiate variables and write header
-    username = ('')
-    domain = ('')
-    at = False
-    header = ['username', 'domain']
     # open file in write mode
     f = open('sliced.csv', 'w', encoding='UTF8')
+
     # create csv writer and write header
     writer = csv.writer(f)
+    header = ['username', 'domain']
     writer.writerow(header)
-    #iterates through characters of email
-    for x in email:
-        #write email[x] to csv if letter is not @
-        if x != '@':
-            # at only true when @ encountered
-            if at == False:
-                username += (f'{x}')
-            if at == True:
-                domain += (f'{x}')
-        elif x == '@':
-        #set at to True if @ is encountered
-            at = True
-            print('@ sign found')
-            pass
 
-    data = (f'{username} {domain}')
-    writer.writerow(data)
+    # loops forever
+    while True:
 
+        # get email
+        email = input('email: ')
+
+        # if no email entered, close the file and break the loop
+        if email == '':
+            f.close()
+            break
+
+        # split email at @ sign
+        email = email.split('@')
+
+        #write email to csv-file
+        writer.writerow(email)
+
+    # close the file
     f.close()
 
-def main():
-    for i in range(0,9):
-        email = input('Email: ')
-        slice(email)
-main()
+# run the function
+slice()
